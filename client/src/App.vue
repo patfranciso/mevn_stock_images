1a2f0a79f835ff46a2c7f45b3d82d094a849fd98
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-22 15:36:34 +0100

    Nav view code

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,31 @@
       v-model="drawer"
       app
     >
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            MERN Stock Images
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            MEVN Full Stack Sample App
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list dense>
+      <v-list-item-group
+        color="primary"
+      >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.link" link>
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -21,6 +45,13 @@
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
-  };
+    data: () => ({ 
+    	drawer: null ,
+    	items: [
+        { title: "Home", icon: "mdi-home", link: "/" },
+        { title: "Add Post", icon: "mdi-note-plus", link: "/add-post" }, 
+        { title: "About", icon: "mdi-help-box", link: "/about" },
+        ],
+    }),
+  }
 </script>
