b37626e714f6096be1d7f742b0e1bb2e43019b5e
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-22 10:46:58 +0100

    API endpoints placeholders

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -1,6 +1,23 @@
-module.exports = class API{
-	// fetch all posts
-	static async fetchAllPost(req, res){
+module.exports = class API {
+  // fetch all posts
+  static async fetchAllPost(req, res) {
     res.send('Hello from API');
   }
+
+  // fetch post by id
+  static async fetchPostById(req, res) {
+    res.send('Fetch Post by ID');
+  }
+  // create a post
+  static async createPost(req, res) {
+    res.send('Create Post');
+  }
+  // update a post
+  static async updatePost(req, res) {
+    res.send('Update Post');
+  }
+  // delete a post
+  static async deletePost(req, res) {
+    res.send('Delete Post');
+  };
 }
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,5 +4,11 @@ const router = express.Router();
 const API = require('../controllers/api');
 
 router.get('/', API.fetchAllPost);
+router.get('/:id', API.fetchPostById);
+router.post('/', API.createPost);
+router.post('/', API.createPost);
+router.patch('/:id', API.updatePost);
+router.patch('/:id', API.updatePost);
+router.delete('/:id', API.deletePost);
 
 module.exports = router;
