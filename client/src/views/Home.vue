875354386d28a4e4e0d2771ad7c92b62819d657f
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-22 15:28:11 +0100

    Base wireframe

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,13 +3,8 @@
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
   }
 </script>
