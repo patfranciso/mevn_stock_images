85e144503bd469aacf7cb87ead3ad927f04701d6
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-22 10:52:17 +0100

    Get all posts endpoint

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -1,7 +1,15 @@
+const Post = require('../models/posts');
+
+
 module.exports = class API {
   // fetch all posts
   static async fetchAllPost(req, res) {
-    res.send('Hello from API');
+    try{
+			const posts = await Post.find();
+			res.status(200).json(posts);
+		}catch(err){
+			res.status(404).json({message: err.message});
+		}
   }
 
   // fetch post by id
