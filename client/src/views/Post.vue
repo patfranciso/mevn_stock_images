7298f7d66911a075c82ed9b3fbae7029afe42a6b
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-23 15:45:31 +0100

    Delete post done

--- client/src/views/Post.vue
+++ client/src/views/Post.vue
@@ -11,7 +11,7 @@
               </v-col>
               <v-col sm="10" class="d-flex justify-end">
                 <v-btn color="success" text :to="{ name: 'edit-post', params: {id: post._id} }">Edit</v-btn>
-                <v-btn color="red" text>Delete</v-btn>
+                <v-btn color="red" text @click="removePost(post._id)">Delete</v-btn>
               </v-col>
             </v-row>
           </v-card-actions>
@@ -40,6 +40,12 @@ export default {
   async created(){
     const response = await API.getPostById(this.$route.params.id);
     this.post = response;
+  },
+  methods:{
+    async removePost(id) {
+      const response = await API.deletePost(id);
+      this.$router.push({ name: 'home', params: { message: response.message}});
+    }
   }
 }
 </script>
