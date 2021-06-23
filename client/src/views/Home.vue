9117666fec4ca4dcbba97b19725a0e6a8268d54e
Patrick Francis Omogbeme <patfranciso@gmail.com>
2021-06-23 22:43:57 +0100

    Image ref update

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container>
-    <v-alert border="left" close-text="Close" color="green accent-4" dark dismissable v-if="this.$route.params.message">
+    <v-alert border="left" close-text="Close Alert" color="green accent-4" dark dismissable v-if="this.$route.params.message">
       {{ this.$route.params.message }}
     </v-alert>
     <v-row no-gutters>
       <v-col sm="4" class="pa-3" v-for="post in posts" :key="post.id">
         <v-card class="pa-1" :to="{ name: 'post', params: {id: post._id} }">
-          <v-img height="250" :src="'http://localhost:5000/images/'+post.image"></v-img>
+          <v-img height="250" :src="`/${post.image}`"></v-img>
           <v-btn class="ml-4 mt-3" small outlined color="indigo">
             {{ post.category }}
           </v-btn>
